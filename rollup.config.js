b162c7159585e7a9f9ec22b881dc5dcbe42d12d5
叨喽 <zhiguo.jzg@alibaba-inc.com>
2022-05-17 14:29:15 +0800

    chore: 🔧 optimize compile

--- rollup.config.js
+++ rollup.config.js
@@ -2,12 +2,11 @@
  * @Author: zhiguo.jzg
  * @Date: 2022-05-17 04:38:57
  * @Description: TODO: Description of file, its uses and information
- * @LastEditTime: 2022-05-17 10:37:51
+ * @LastEditTime: 2022-05-17 14:24:49
  * @LastEditors: zhiguo.jzg
  */
 import ts from 'rollup-plugin-ts';
-import copy from 'rollup-plugin-copy';
-import { terser } from 'rollup-plugin-terser';
+// import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/index.ts',
@@ -20,7 +19,6 @@ export default {
     ts({
       tsconfig: 'tsconfig.json',
     }),
-    copy({ targets: [{ src: 'runtime/*', dest: 'lib/runtime' }] }),
-    terser(),
+    // terser(),
   ],
 };
